961881d9a8a344a5454a319dd7f1fd8ceecb0c26
pezholio <pezholio@gmail.com>
2013-11-23 19:45:21 +0000

    Moar bash

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,2 +1,4 @@
+echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
 sudo apt-get update
 sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis
+sudo apt-get install -qq libgeos++-dev
