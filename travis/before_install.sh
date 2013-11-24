dcae4cfb7fdb087324c3926ac34376504073de3d
pezholio <pezholio@gmail.com>
2013-11-24 17:52:04 +0000

    Try again

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,2 +1,4 @@
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" >> /etc/apt/sources.list'
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-9.1-postgis
+sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis
