802dc72fce2309d8ad6c0d950cefdab78aaaaedd
pezholio <pezholio@gmail.com>
2013-11-24 11:03:15 +0000

    Let's try this

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,4 +1,17 @@
-echo "yes" | sudo apt-add-repository ppa:ubuntugis/ubuntugis-unstable
+sudo /etc/init.d/postgresql stop
+sudo cp /etc/postgresql/9.1/main/pg_hba.conf ./
+sudo apt-get remove postgresql postgresql-9.1 -qq --purge
+source /etc/lsb-release
+echo "deb http://apt.postgresql.org/pub/repos/apt/ $DISTRIB_CODENAME-pgdg main" > pgdg.list
+sudo mv pgdg.list /etc/apt/sources.list.d/
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.1-postgis
-sudo apt-get install -qq libgeos++-dev
+sudo apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install postgresql-9.2 postgresql-contrib-9.2 -qq
+sudo /etc/init.d/postgresql stop
+sudo cp ./pg_hba.conf /etc/postgresql/9.2/main
+sudo /etc/init.d/postgresql start
+psql -c 'create database test;' -U postgres
+sudo apt-add-repository -y ppa:ubuntugis/ubuntugis-unstable
+sudo apt-get update
+sudo apt-get install -qq libgeos-dev libproj-dev postgresql-9.2-postgis
+psql -d test -c 'CREATE EXTENSION postgis;' -U postgres
