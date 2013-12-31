dbf49c10b9cebe7e5e46ded9508b09bd28d85750
pezholio <pezholio@gmail.com>
2013-12-31 08:26:07 +0000

    Cache static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
+  caches_page :static
+  
   def render_404
     respond_to do |format|
       format.html { render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found }
