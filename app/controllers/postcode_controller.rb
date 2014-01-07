cdba18ddd2b948bde1b6d3c75295d402aab7e34e
pezholio <pezholio@gmail.com>
2014-01-07 08:04:32 +0000

    Order by distance

--- app/controllers/postcode_controller.rb
+++ app/controllers/postcode_controller.rb
@@ -74,7 +74,9 @@ class PostcodeController < ApplicationController
     
     params[:format] ||= "html"
     
-    postcode = Postcode.where("ST_DWithin(latlng, 'POINT(#{params[:lat].to_f} #{params[:lng].to_f})', 1609.344)").first
+    postcode = Postcode.where("ST_DWithin(latlng, 'POINT(#{params[:lat].to_f} #{params[:lng].to_f})', 1609.344)")
+                              .order("ST_Distance(latlng, 'POINT(#{params[:lat].to_f} #{params[:lng].to_f})')")
+                              .first
     p = postcode.postcode.gsub(" ", "")
     redirect_to postcode_url(p, format: params[:format]), status: "303"
   end
