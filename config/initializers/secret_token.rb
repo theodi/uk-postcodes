812d8171a36bb3cfe4a2d2c5c94e6154153ed015
pezholio <pezholio@gmail.com>
2014-01-09 13:54:38 +0000

    Remove secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-UkPostcodes::Application.config.secret_token = '2f3ead8131ce9f4a162ed195cdfe53a0a55ddcd3671734893c9e12cab4ab0c512cd2ef73632d29b5d4209b8992971a6da9fe3a1745caa7cd91bab01b07f97c6b'
+UkPostcodes::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30)
+else
+  ENV['SECRET_TOKEN']
+end
