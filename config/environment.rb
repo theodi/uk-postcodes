380602dcc5862d3e4ccd83832e4cc10732dd4a96
pezholio <pezholio@gmail.com>
2014-03-13 14:22:16 +0000

    Add N-Triples

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 Mime::Type.register "application/rdf+xml", :rdf
+Mime::Type.register "text/n3", :n3
 
 # Initialize the rails application
 UkPostcodes::Application.initialize!
